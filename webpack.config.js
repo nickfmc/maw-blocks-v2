756d57813466e8de19eb15ed6dd7a7f6c89686f7
nickfmc <nick@mountainairweb.com>
2025-10-04 13:34:26 -0700

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,21 @@
 
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     ...defaultConfig,
     entry: {
         'blocks/video/index': './blocks/video/index.js',
-        'blocks/video/style': './blocks/video/style.scss',
+        'blocks/video/style-style': './blocks/video/style.scss',
         'blocks/video/editor': './blocks/video/editor.scss',
         'blocks/video/frontend': './blocks/video/frontend.js',
         'blocks/slider/index': './blocks/slider/index.js',
-        'blocks/slider/style': './blocks/slider/style.scss',
+        'blocks/slider/style-style': './blocks/slider/style.scss',
         'blocks/slider/editor': './blocks/slider/editor.scss',
         'blocks/slider/frontend': './blocks/slider/frontend.js',
         'blocks/testimonial-slider/index': './blocks/testimonial-slider/index.js',
-        'blocks/testimonial-slider/style': './blocks/testimonial-slider/style.scss',
+        'blocks/testimonial-slider/style-style': './blocks/testimonial-slider/style.scss',
         'blocks/testimonial-slider/editor': './blocks/testimonial-slider/editor.scss',
         'blocks/testimonial-slider/frontend': './blocks/testimonial-slider/frontend.js',
         'editor': './assets/scss/editor.scss'
@@ -26,6 +27,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
-    }
+    },
+    plugins: [
+        ...defaultConfig.plugins.filter(
+            plugin => !(plugin instanceof MiniCssExtractPlugin)
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
 };
 
